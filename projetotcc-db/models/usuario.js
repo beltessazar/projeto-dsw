e4ecb9598603f47bd181d79e46ef3cb970e0fb07
Daniel Moraes <dsm0ra3s@gmail.com>
2020-09-11 18:36:46 -0300

    incluindo pasta com acesso ao banco

---
+++ projetotcc-db/models/usuario.js
@@ -0,0 +1,142 @@
+const {
+  DataTypes
+} = require('sequelize');
+
+module.exports = sequelize => {
+  const attributes = {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      defaultValue: null,
+      primaryKey: true,
+      autoIncrement: false,
+      comment: null,
+      field: "id",
+      unique: "id"
+    },
+    dataNascimento: {
+      type: DataTypes.DATEONLY,
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "dataNascimento"
+    },
+    nacionalidade: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "nacionalidade"
+    },
+    endereco: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "endereco"
+    },
+    bairro: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "bairro"
+    },
+    cidade: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "cidade"
+    },
+    telefone: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "telefone"
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "email"
+    },
+    nomePai: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "nomePai"
+    },
+    nomaMae: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "nomaMae"
+    },
+    profissaoPai: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "profissaoPai"
+    },
+    profissaoMae: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "profissaoMae"
+    },
+    grupoId: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "grupoId",
+      references: {
+        key: "id",
+        model: "gruposModel"
+      }
+    }
+  };
+  const options = {
+    tableName: "usuario",
+    comment: "",
+    indexes: [{
+      name: "grupoId",
+      unique: false,
+      type: "BTREE",
+      fields: ["grupoId"]
+    }]
+  };
+  const UsuarioModel = sequelize.define("usuarioModel", attributes, options);
+  return UsuarioModel;
+};
